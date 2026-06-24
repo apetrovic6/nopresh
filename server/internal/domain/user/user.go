222246075b04ac5f3a025fed9a3f4b785eced5f6
apetrovic <petrovicante6@gmail.com>
2026-06-24 16:28:02 +0200

    feat(user): add domain layer

---
+++ server/internal/domain/user/user.go
@@ -0,0 +1,22 @@
+package user
+
+import (
+	"time"
+)
+
+type User struct {
+	ID             uint
+	Name           string
+	Email          string
+	Hashed_pasword string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	Version        int
+}
+
+func New(email, name string) *User {
+	return &User{
+		Name:  name,
+		Email: email,
+	}
+}
