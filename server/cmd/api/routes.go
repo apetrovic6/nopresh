8ef8f50e4b242184f1d4f5c96b90c90705161ea6
apetrovic <petrovicante6@gmail.com>
2026-07-17 19:25:04 +0200

    refactor(backend): remove greet dummy proto and service

--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -1,42 +1,20 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
 	"connectrpc.com/authn"
 	"connectrpc.com/connect"
 	"connectrpc.com/validate"
 
-	greetv1 "nopresh.apetrovic.com/gen/greet/v1"
-	"nopresh.apetrovic.com/gen/greet/v1/greetv1connect"
 	"nopresh.apetrovic.com/gen/proto/auth/v1/authv1connect"
 	"nopresh.apetrovic.com/gen/proto/bloodpressure/v1/bloodpressurev1connect"
 	"nopresh.apetrovic.com/gen/proto/medication/v1/medicationv1connect"
 	"nopresh.apetrovic.com/gen/proto/settings/v1/settingsv1connect"
 )
 
-type GreetServer struct{}
-
-func (s *GreetServer) Greet(
-	_ context.Context,
-	req *connect.Request[greetv1.GreetRequest],
-) (*connect.Response[greetv1.GreetResponse], error) {
-
-	res := connect.NewResponse(&greetv1.GreetResponse{
-		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
-	})
-
-	return res, nil
-}
-
 func (app *app) routes() http.Handler {
 	router := http.NewServeMux()
-
-	router.Handle(greetv1connect.NewGreetServiceHandler(&GreetServer{},
-		connect.WithInterceptors(validate.NewInterceptor()),
-	))
 
 	router.Handle(authv1connect.NewAuthServiceHandler(&AuthServer{models: app.models, logger: app.logger, jwt: app.jwt},
 		connect.WithInterceptors(validate.NewInterceptor()),
